2859c9c93a737a21ece6214c598d777d621b7743
ShuaiBin <3166688833@qq.com>
2019-06-01 23:52:29 +0800

    init

---
+++ src/main/java/com/binzikeji/itoken/zipkin/ZipKinApplication.java
@@ -0,0 +1,21 @@
+package com.binzikeji.itoken.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @Description
+ * @Author Bin
+ * @Date 2019/4/3 11:16
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class, args);
+    }
+}
